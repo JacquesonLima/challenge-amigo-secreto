0c2c9a4506d6458bde532908fcd7518d9347ce44
JacquesonLima <jacquesonbenevides@gmail.com>
2025-01-30 10:04:58 -0300

    função de adicionar amigo criada.

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let friendList = [];
+
+function adicionarAmigo() {
+  let friendName = document.getElementById("amigo").value;
+  friendName.value = "";
+  return friendList.push(friendName);
+}
